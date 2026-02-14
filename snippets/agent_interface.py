cf5da0fbb2602d86f286677b34ecf5ee3fb339c6
Anant Jain <134965818+inf14@users.noreply.github.com>
2026-02-15 01:54:10 +0530

    Create EnergyAwareAgent interface with abstract methods

---
+++ snippets/agent_interface.py
@@ -0,0 +1,14 @@
+class EnergyAwareAgent:
+    """
+    Abstract agent interface.
+    Implementation details are intentionally omitted.
+    """
+
+    def observe(self, state):
+        pass
+
+    def evaluate_actions(self, actions):
+        pass
+
+    def select_action(self):
+        pass
